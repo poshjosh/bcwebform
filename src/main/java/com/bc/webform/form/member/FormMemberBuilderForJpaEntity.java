3fb967a3e32dbf23c412c0558324213aebd62c3e
poshjosh <posh.bc@gmail.com>
2020-07-24 15:14:29 +0100

    Bump to version 1.1.0

--- src/main/java/com/bc/webform/form/member/FormMemberBuilderForJpaEntity.java
+++ src/main/java/com/bc/webform/form/member/FormMemberBuilderForJpaEntity.java
@@ -1,5 +1,6 @@
 package com.bc.webform.form.member;
 
+import com.bc.webform.TypeTestsImpl;
 import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,13 +15,13 @@ public class FormMemberBuilderForJpaEntity extends FormMemberBuilderForPojo{
     private static final Logger LOG = Logger.getLogger(FormMemberBuilderForJpaEntity.class.getName());
 
     public FormMemberBuilderForJpaEntity() { 
-        this(new FormInputContextForJpaEntity());
+        this(new FormInputContextForJpaEntity(), new MultiChoiceContextForPojo(new TypeTestsImpl()));
     }
     
     public FormMemberBuilderForJpaEntity(
-            FormInputContext<Object, Field, Object> formInputContext) { 
-    
-        this.formInputContext(formInputContext);
+            FormInputContext<Object, Field, Object> formInputContext,
+            MultiChoiceContext<Object, Field> multiChoiceContext) { 
+        super(formInputContext, multiChoiceContext);
     }
 
     @Override
