fc9fe07e47f77ff00ea7db77b9cca526d14b9e60
poshjosh <posh.bc@gmail.com>
2020-07-28 15:41:56 +0100

    Add the field value as third argument to method getValue

--- src/main/java/com/bc/webform/form/member/MultiChoiceContext.java
+++ src/main/java/com/bc/webform/form/member/MultiChoiceContext.java
@@ -9,11 +9,11 @@ import java.util.List;
  */
 public interface MultiChoiceContext<S, F> {
     
-    MultiChoiceContext NO_OP = (s, f) -> Collections.EMPTY_LIST;
+    MultiChoiceContext NO_OP = (s, f, v) -> Collections.EMPTY_LIST;
     
     default boolean isMultiChoice(S formDataSource, F dataSourceField) {
         return false;
     }
     
-    List<SelectOption> getChoices(S formDataSource, F dataSourceField); 
+    List<SelectOption> getChoices(S formDataSource, F dataSourceField, Object fieldValue); 
 }
