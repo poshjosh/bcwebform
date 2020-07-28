b712d9f16dc10b408218e640498e18f1a120b9d2
poshjosh <posh.bc@gmail.com>
2020-07-28 15:42:21 +0100

    Remove EnumType constructor argument.

--- src/main/java/com/bc/webform/form/member/MultiChoiceContextForPojo.java
+++ src/main/java/com/bc/webform/form/member/MultiChoiceContextForPojo.java
@@ -8,73 +8,98 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.BiFunction;
-import javax.persistence.EnumType;
 
 /**
  * @author hp
  */
 public class MultiChoiceContextForPojo implements MultiChoiceContext<Object, Field>{
+    
+    private static class Printer implements BiFunction<Object, Locale, String>{
+        @Override
+        public String apply(Object entity, Locale locale) {
+            // toString will return null for generic enums
+            if(entity instanceof Enum) {
+                return entity.toString() != null ? entity.toString() : ((Enum)entity).name();
+            }else{
+                return String.valueOf(entity);
+            }
+        }
+    }
 
     private final TypeTests typeTests;
-    private final EnumType enumType;
-    private final BiFunction<Enum, Locale, Object> printer;
+    private final BiFunction<Object, Locale, String> printer;
     private final Locale locale;
 
     public MultiChoiceContextForPojo(TypeTests typeTests) {
-        this(typeTests, EnumType.STRING, (en, loc) -> en.toString(), Locale.getDefault());
+        this(typeTests, new Printer(), Locale.getDefault());
     }
     
     public MultiChoiceContextForPojo(
             TypeTests typeTests,
-            EnumType enumType,
-            BiFunction<Enum, Locale, Object> printer, 
+            BiFunction<Object, Locale, String> printer, 
             Locale locale) {
         this.typeTests = Objects.requireNonNull(typeTests);
-        this.enumType = Objects.requireNonNull(enumType);
         this.printer = Objects.requireNonNull(printer);
         this.locale = Objects.requireNonNull(locale);
     }
 
     @Override
     public boolean isMultiChoice(Object source, Field field) {
-        return typeTests.isEnumType(field.getType());
+        return typeTests.isEnumType(field.getType()) || typeTests.isDomainType(field.getType());
     }
     
     @Override
-    public List<SelectOption> getChoices(Object source, Field field) {
+    public List<SelectOption> getChoices(Object source, Field field, Object fieldValue) {
         final Class fieldType = field.getType();
         if(this.typeTests.isEnumType(fieldType)) {
-            return this.getEnumChoices(fieldType);
+            return this.getEnumChoices(source, field, fieldValue);
+        }else if(this.typeTests.isDomainType(fieldType)){
+            return this.getObjectChoices(source, field, fieldValue);
         }else{
             return null;
         }
     }
     
-    public List<SelectOption> getEnumChoices(Class type) {
+    public List<SelectOption> getEnumChoices(Object source, Field field, Object fieldValue) {
+        final Class type = field.getType();
         if(this.typeTests.isEnumType(type)) {
             BiFunction format = this.printer;
-            switch(enumType) {
-                case STRING:
-                    return SelectOptions.fromEnumName(type, locale, format);
-                case ORDINAL:
-                    return SelectOptions.fromEnumOrdinal(type, locale, format);
-                default:
-                    throw new IllegalArgumentException("Unexpected javax.persistence.EnumType: "  + enumType);
-            }        
+            return SelectOptions.fromEnumOrdinal(type, locale, format);
         }
-        return Collections.EMPTY_LIST;
+        throw new IllegalArgumentException();
     }
 
+    public List<SelectOption> getObjectChoices(Object source, Field field, Object fieldValue) {
+        final Class fieldType = field.getType();
+        if(this.typeTests.isDomainType(fieldType)){
+            return this.getFieldValueChoice(source, field, fieldValue)
+                    .map((option) -> Collections.singletonList(option))
+                    .orElse(null);
+        }
+        throw new IllegalArgumentException();
+    }
+    
+    public Optional<SelectOption> getFieldValueChoice(
+            Object source, Field field, Object fieldValue) {
+        final Class fieldType = field.getType();
+        if(this.typeTests.isDomainType(fieldType)) {
+            if(fieldValue != null) {
+                SelectOption option = SelectOptions.from(fieldValue, printer.apply(fieldValue, locale));
+                return Optional.of(option);
+            }else{
+                return Optional.empty();
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+    
     public TypeTests getTypeTests() {
         return typeTests;
     }
 
-    public EnumType getEnumType() {
-        return enumType;
-    }
-
-    public BiFunction<Enum, Locale, Object> getPrinter() {
+    public BiFunction<Object, Locale, String> getPrinter() {
         return printer;
     }
 
