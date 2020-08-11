38899ad0cd4cae70ab3a85388e790626cebbf5f4
poshjosh <posh.bc@gmail.com>
2020-08-11 19:23:24 +0100

    Add annotations to mark fields for their form inpu type e.g @Hidden

--- src/main/java/com/bc/webform/form/member/FormInputTypeProviderForPojo.java
+++ src/main/java/com/bc/webform/form/member/FormInputTypeProviderForPojo.java
@@ -1,13 +1,17 @@
 package com.bc.webform.form.member;
 
+import com.bc.webform.form.member.annotations.Hidden;
 import com.bc.webform.StandardFormFieldTypes;
+import com.bc.webform.form.member.annotations.Checkbox;
+import com.bc.webform.form.member.annotations.File;
+import com.bc.webform.form.member.annotations.Password;
+import com.bc.webform.form.member.annotations.Radio;
 import com.bc.webform.functions.IsPasswordField;
 import java.lang.reflect.Field;
 import java.time.LocalTime;
 import java.time.OffsetTime;
 import java.util.Objects;
 import java.util.function.Predicate;
-import java.util.logging.Logger;
 
 /**
  * @author hp
@@ -15,7 +19,7 @@ import java.util.logging.Logger;
 public class FormInputTypeProviderForPojo 
         implements FormInputTypeProvider<Object, Field>{
     
-    private static final Logger LOG = Logger.getLogger(FormInputTypeProviderForPojo.class.getName());
+//    private static final Logger LOG = Logger.getLogger(FormInputTypeProviderForPojo.class.getName());
 
     private final String resultIfNone;
     
@@ -38,33 +42,49 @@ public class FormInputTypeProviderForPojo
     
     @Override
     public String getType(Object source, Field field) {
-        final String result;
+        String result = null;
         final Class cls = field.getType();
-        if(passwordFieldTest.test(field.getName())) {
-            result = StandardFormFieldTypes.PASSWORD;  
-        }else if(CharSequence.class.isAssignableFrom(cls)) {
-            result = StandardFormFieldTypes.TEXT;
-        }else if(cls == Boolean.class || cls == boolean.class) {
-            result = StandardFormFieldTypes.CHECKBOX;
-        }else if(LocalTime.class.isAssignableFrom(cls) || 
-                OffsetTime.class.isAssignableFrom(cls) ||
-                java.sql.Time.class.isAssignableFrom(cls)) {
-            result = StandardFormFieldTypes.TIME;
-        }else if(java.time.ZonedDateTime.class.isAssignableFrom(cls) ||
-                java.time.LocalDateTime.class.isAssignableFrom(cls) ||
-                java.time.OffsetDateTime.class.isAssignableFrom(cls) ||
-                java.sql.Timestamp.class.isAssignableFrom(cls)){
-            result = StandardFormFieldTypes.DATETIME;
-        }else if(java.util.Date.class.isAssignableFrom(cls) ||
-                java.time.LocalDate.class.isAssignableFrom(cls)){
-            result = StandardFormFieldTypes.DATE;
-        }else if(Number.class.isAssignableFrom(cls)) {
-            result = StandardFormFieldTypes.NUMBER;
-        }else{
-            result = null;
+        final Hidden hidden = field.getAnnotation(Hidden.class);
+        if(hidden != null && hidden.value() == true) {
+            result = StandardFormFieldTypes.HIDDEN;
         }
-        
-        LOG.finer(() -> "Input: " + cls + ", output: " + result);
+        if(result == null) {
+            result = field.getAnnotation(Checkbox.class) == null ? null : StandardFormFieldTypes.CHECKBOX;
+        }
+        if(result == null) {
+            result = field.getAnnotation(File.class) == null ? null : StandardFormFieldTypes.FILE;
+        }
+        if(result == null) {
+            result = field.getAnnotation(Password.class) == null ? null : StandardFormFieldTypes.PASSWORD;
+        }
+        if(result == null) {
+            result = field.getAnnotation(Radio.class) == null ? null : StandardFormFieldTypes.RADIO;
+        }
+        if(result == null){
+            if(passwordFieldTest.test(field.getName())) {
+                result = StandardFormFieldTypes.PASSWORD;  
+            }else if(CharSequence.class.isAssignableFrom(cls)) {
+                result = StandardFormFieldTypes.TEXT;
+            }else if(cls == Boolean.class || cls == boolean.class) {
+                result = StandardFormFieldTypes.CHECKBOX;
+            }else if(LocalTime.class.isAssignableFrom(cls) || 
+                    OffsetTime.class.isAssignableFrom(cls) ||
+                    java.sql.Time.class.isAssignableFrom(cls)) {
+                result = StandardFormFieldTypes.TIME;
+            }else if(java.time.ZonedDateTime.class.isAssignableFrom(cls) ||
+                    java.time.LocalDateTime.class.isAssignableFrom(cls) ||
+                    java.time.OffsetDateTime.class.isAssignableFrom(cls) ||
+                    java.sql.Timestamp.class.isAssignableFrom(cls)){
+                result = StandardFormFieldTypes.DATETIME;
+            }else if(java.util.Date.class.isAssignableFrom(cls) ||
+                    java.time.LocalDate.class.isAssignableFrom(cls)){
+                result = StandardFormFieldTypes.DATE;
+            }else if(Number.class.isAssignableFrom(cls)) {
+                result = StandardFormFieldTypes.NUMBER;
+            }else{
+                result = null;
+            }
+        }
         
         return result == null ? resultIfNone : result;
     }
---
+++ src/main/java/com/bc/webform/form/member/annotations/Password.java
@@ -0,0 +1,13 @@
+package com.bc.webform.form.member.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author hp
+ */
+@Target(value = {ElementType.ANNOTATION_TYPE, ElementType.FIELD})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Password { }
